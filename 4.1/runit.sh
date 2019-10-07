8d31476c8872088077fe698350aa2b5684d3b817
glennswest <gwest@redhat.com>
2019-10-07 17:50:45 -0500

    Change node/master vm size

--- 4.1/runit.sh
+++ 4.1/runit.sh
@@ -1,10 +1,7 @@
-#rm -r -f gw
-#mkdir gw
-#cp install-config.yaml gw
-#openshift-install create ignition-configs --dir=gw
-
-#cp ~/gw.lo/gw/worker.ign ~
-#rm -f ~/.kube/config
+rm -r -f gw
+mkdir gw
+cp install-config.yaml gw
+openshift-install create ignition-configs --dir=gw
 #cp gw/auth/kubeconfig ~/.kube/config
 #rm ~/.kube/config
 #cp gw/auth/kubeconfig ~/.kube/config
@@ -15,9 +12,21 @@
 
 
 #~/azlogin.sh
-#az group delete --name gswx1
-#az group create --name gswx1 --location "East US"
+echo "Delete old resource group"
+az group delete --name gswx1 --yes
+echo "Create new resource group"
+az group create --name gswx1 --location "East US"
+echo "Copy RHCOS to resource group"
+export VHD_NAME=rhcos-410.8.20190504.0-azure.vhd
+#az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1 --sku Premium_LRS
+az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1 
+az storage container create --name vhd --account-name sagswx1
+ACCOUNT_KEY=$(az storage account keys list --account-name sagswx1 --resource-group gswx1 --query "[0].value" -o tsv)
+az storage blob copy start --account-name "sagswx1" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME"
+
+echo "Configure template with ignition files"
 python setup-variables.py
+echo "Start Deployment"
 az group deployment create \
    --name gswx1 \
    --resource-group gswx1 \
