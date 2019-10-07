e67ac52a566f1754e32874bf8706fa27df611aa8
glennswest <gwest@redhat.com>
2019-10-07 16:54:42 -0500

    Change storage account naming

--- 4.1/copyvhd.sh
+++ 4.1/copyvhd.sh
@@ -1,7 +1,6 @@
 export VHD_NAME=rhcos-410.8.20190504.0-azure.vhd
-az storage account create --location "East US" --name ckocp4storage --kind StorageV2 --resource-group gswx1
-az storage container create --name vhd --account-name ckocp4storage
-#az group create --location uksouth --name rhcos_images
-ACCOUNT_KEY=$(az storage account keys list --account-name ckocp4storage --resource-group gswx1 --query "[0].value" -o tsv)
-az storage blob copy start --account-name "ckocp4storage" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME"
+az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1
+az storage container create --name vhd --account-name sagswx1
+ACCOUNT_KEY=$(az storage account keys list --account-name sagswx1 --resource-group gswx1 --query "[0].value" -o tsv)
+az storage blob copy start --account-name "sagswx1" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME"
 
