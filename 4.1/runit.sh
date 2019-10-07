e8e00a36c2500af469c2ea8badc950419bb13f8c
glennswest <gwest@redhat.com>
2019-10-07 18:05:21 -0500

    Change sa type

--- 4.1/runit.sh
+++ 4.1/runit.sh
@@ -19,7 +19,7 @@ az group create --name gswx1 --location "East US"
 echo "Copy RHCOS to resource group"
 export VHD_NAME=rhcos-410.8.20190504.0-azure.vhd
 #az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1 --sku Premium_LRS
-az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1 
+az storage account create --location "East US" --name sagswx1 --kind Storage --resource-group gswx1  --sku Standard_LRS
 az storage container create --name vhd --account-name sagswx1
 ACCOUNT_KEY=$(az storage account keys list --account-name sagswx1 --resource-group gswx1 --query "[0].value" -o tsv)
 az storage blob copy start --account-name "sagswx1" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME"
