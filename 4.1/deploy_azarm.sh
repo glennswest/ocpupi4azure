c413dab02560f7b57574ae4e68cf5da3d38953a6
glennswest <gwest@redhat.com>
2019-10-16 08:42:59 -0500

    Make scripts to use paramter for resource group name

--- 4.1/deploy_azarm.sh
+++ 4.1/deploy_azarm.sh
@@ -1,7 +1,7 @@
 echo "Start Deployment"
 az group deployment create \
-   --name gswx1 \
-   --resource-group gswx1 \
+   --name $1 \
+   --resource-group $1 \
    --template-uri "https://raw.githubusercontent.com/glennswest/ocpupi4azure/master/4.1/azuredeploy.json" \
      --parameters "runit.parameters.json"
 
--- 4.1/setup_azarm.sh
+++ 4.1/setup_azarm.sh
@@ -1,3 +1,4 @@
+echo "Using resource group $1
 rm -r -f gw
 mkdir gw
 cp install-config.yaml gw
@@ -13,31 +14,31 @@ openshift-install create ignition-configs --dir=gw
 
 #~/azlogin.sh
 echo "Delete old resource group"
-az group delete --name gswx1 --yes
+az group delete --name $1 --yes
 echo "Create new resource group"
-az group create --name gswx1 --location "East US"
+az group create --name $1 --location "East US"
 echo "Copy RHCOS to resource group"
 export VHD_NAME=rhcos-410.8.20190504.0-azure.vhd
-#az storage account create --location "East US" --name sagswx1 --kind StorageV2 --resource-group gswx1 --sku Premium_LRS
-az storage account create --location "East US" --name sagswx1 --kind Storage --resource-group gswx1  --sku Standard_LRS
-az storage container create --name vhd --account-name sagswx1
-export ACCOUNT_KEY=$(az storage account keys list --account-name sagswx1 --resource-group gswx1 --query "[0].value" -o tsv)
-az storage blob copy start --account-name "sagswx1" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME" 
+#az storage account create --location "East US" --name sa${1} --kind StorageV2 --resource-group $1 --sku Premium_LRS
+az storage account create --location "East US" --name sa${1} --kind Storage --resource-group $1  --sku Standard_LRS
+az storage container create --name vhd --account-name sa${1}
+export ACCOUNT_KEY=$(az storage account keys list --account-name sa${1} --resource-group $1 --query "[0].value" -o tsv)
+az storage blob copy start --account-name "sa${1}" --account-key "$ACCOUNT_KEY" --destination-blob "$VHD_NAME" --destination-container vhd --source-uri "https://openshifttechpreview.blob.core.windows.net/rhcos/$VHD_NAME" 
 echo "Waiting on copy of vhd"
 status="unknown"
 echo $status
 while [ "$status" != "success" ]
     do
-    status=$(az storage blob show --container-name vhd --name $VHD_NAME --account-name "sagswx1"  --account-key "$ACCOUNT_KEY" -o json --query properties.copy.status | sed -e 's/^"//' -e 's/"$//')
+    status=$(az storage blob show --container-name vhd --name $VHD_NAME --account-name "sa${1}"  --account-key "$ACCOUNT_KEY" -o json --query properties.copy.status | sed -e 's/^"//' -e 's/"$//')
     done
 echo "Copy of vhd complete"
 
 
 echo "Configure template with ignition files"
-az storage container create --name files --account-name sagswx1 --public-access blob
-ACCOUNT_KEY=$(az storage account keys list --account-name sagswx1 --resource-group gswx1 --query "[0].value" -o tsv)
-az storage blob upload --account-name sagswx1 --account-key $ACCOUNT_KEY -c "files" -f "gw/bootstrap.ign" -n "bootstrap.ign"
-BOOTSTRAPURL=$(az storage blob url --account-name sagswx1 --account-key $ACCOUNT_KEY -c "files" -n "bootstrap.ign" -o tsv)
+az storage container create --name files --account-name sa${1} --public-access blob
+ACCOUNT_KEY=$(az storage account keys list --account-name sa${1} --resource-group $1 --query "[0].value" -o tsv)
+az storage blob upload --account-name sa${1} --account-key $ACCOUNT_KEY -c "files" -f "gw/bootstrap.ign" -n "bootstrap.ign"
+BOOTSTRAPURL=$(az storage blob url --account-name sa${1} --account-key $ACCOUNT_KEY -c "files" -n "bootstrap.ign" -o tsv)
 python setup-variables.py $BOOTSTRAPURL
 
-az network public-ip create -g gswx1 -n gswx1 --allocation-method static
+az network public-ip create -g $1 -n $1 --allocation-method static
