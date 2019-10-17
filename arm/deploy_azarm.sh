c7384ad918ee6b51356e0198b1c125618a261301
glennswest <gwest@redhat.com>
2019-10-16 19:32:30 -0500

    Add wait for bootstrap complete and poweroff bootstrap vm

--- arm/deploy_azarm.sh
+++ arm/deploy_azarm.sh
@@ -4,4 +4,7 @@ az group deployment create \
    --resource-group $1 \
    --template-uri "https://raw.githubusercontent.com/glennswest/ocpupi4azure/master/arm/azuredeploy.json" \
      --parameters "runit.parameters.json"
-
+openshift-install --dir=gw wait-for bootstrap-complete --log-level debug
+az vm stop --resource-group $1 --name bootstrap-0
+az vm deallocate --resource-group $1 --name bootstrap-0 --no-wait
+openshift-install --dir=gw wait-for install-complete --log-level debug
