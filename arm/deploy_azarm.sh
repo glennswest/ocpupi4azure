af2baebc1ba14085dd6f1a75f5aa8841a791b2e1
glennswest <gwest@redhat.com>
2020-01-07 12:40:57 -0600

    Fix bootstrap dns entry

--- arm/deploy_azarm.sh
+++ arm/deploy_azarm.sh
@@ -15,9 +15,11 @@ echo "Adding internal entries for etcd"
 export MASTER1IP=`az vm show -g $1 -n master1 -d --query privateIps --output tsv`
 export MASTER2IP=`az vm show -g $1 -n master2 -d --query privateIps --output tsv`
 export MASTER3IP=`az vm show -g $1 -n master3 -d --query privateIps --output tsv`
+export BOOTSTRAPIP=az vm show -g $1 -n ${1}m1 -d --query privateIps --output tsv`
 az network private-dns record-set a add-record -g $1 -z $2 -n etcd-0 -a ${MASTER1IP}
 az network private-dns record-set a add-record -g $1 -z $2 -n etcd-1 -a ${MASTER2IP}
 az network private-dns record-set a add-record -g $1 -z $2 -n etcd-2 -a ${MASTER3IP}
+az network private-dns record-set a add-record -g $1 -z $2 -n ${1}m1 -a ${BOOTSTRAPIP}
 ./openshift-install --dir=gw wait-for bootstrap-complete --log-level debug
 az vm stop --resource-group $1 --name bootstrap-0
 az vm deallocate --resource-group $1 --name bootstrap-0 --no-wait
