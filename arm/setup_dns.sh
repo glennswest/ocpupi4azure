cff18acde2fa6e168ef50057e3349ea31104696f
glennswest <gwest@redhat.com>
2020-01-13 08:31:37 -0600

    Change dns settings

--- arm/setup_dns.sh
+++ arm/setup_dns.sh
@@ -10,26 +10,24 @@ az network vnet create \
   --subnet-name nodeSubnet \
   --subnet-prefixes 10.0.1.0/24 \
   --subnet-name masterSubnet \
-  --subnet-prefixes 10.0.0.0/24
-echo "Creating private dns settings"
-az network private-dns zone create -g $1 -n $2
-az network private-dns link vnet create -g $1 -n ${1}DNSLink -z $2 -v openshiftVnet -e true
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master1.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master2.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master3.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t bootstrap-0.${2} -p 1 -w 1 -r 2380
+  --subnet-prefixes 10.0.0.0/24 \
+  --dns-servers
+echo "Creating Public DNS"
+az network dns zone create -g $1 -n $2 --registration-vnets "openshiftVnet" --resolution-vnets "openshiftVnet"
+az network dns link vnet create -g $1 -n ${1}DNSLink -z $2 -v openshiftVnet -e true
+az network dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master1.${2} -p 1 -w 1 -r 2380
+az network dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master2.${2} -p 1 -w 1 -r 2380
+az network dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master3.${2} -p 1 -w 1 -r 2380
+az network dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t bootstrap-0.${2} -p 1 -w 1 -r 2380
 export MASTERIP=`az network public-ip show --resource-group $1 --name $1 --query [ipAddress] --output tsv`
 export APPIP=`az network public-ip show --resource-group $1 --name ${1}app --query [ipAddress] --output tsv`
-az network private-dns record-set a add-record -g $1 -z $2 -n api -a ${MASTERIP}
-az network private-dns record-set a add-record -g $1 -z $2 -n *.apps -a ${APPIP}
 #export INTLBIP=`az network lb frontend-ip show -g $1 --lb-name ${1}intlb -n LoadBalancerFrontEnd --query [privateIpAddress] --output tsv`
 export INTLBIP="10.0.0.63"
-az network private-dns record-set a add-record -g $1 -z $2 -n api-int -a ${INTLBIP}
-az network private-dns record-set list -g gswx1 -z gw.ncc9.com
-echo "Creating Public DNS"
+az network dns record-set a add-record -g $1 -z $2 -n api-int -a ${INTLBIP}
+az network dns record-set list -g gswx1 -z gw.ncc9.com
 az network dns zone create -g $1 -n $2
 az network dns record-set a add-record -g $1 -z $2 -n api -a ${MASTERIP}
 az network dns record-set a add-record -g $1 -z $2 -n *.apps -a ${APPIP}
 export DNSSERVNAME=`az network dns record-set ns show --resource-group $1 --zone-name $2 --name @ --query "nsRecords[0].nsdname" --output tsv`
-az network private-dns record-set list -g gswx1 -z gw.ncc9.com
+az network dns record-set list -g gswx1 -z gw.ncc9.com
 
