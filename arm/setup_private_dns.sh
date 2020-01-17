4f51d0e22254cc8a33c6267c3011099754522e53
glennswest <gwest@redhat.com>
2020-01-17 16:58:08 -0600

    Working azure private dns for 4.3

--- arm/setup_private_dns.sh
+++ arm/setup_private_dns.sh
@@ -13,10 +13,10 @@ az network vnet create \
   --subnet-name masterSubnet \
   --subnet-prefixes 10.0.0.0/24 
 az network private-dns zone create -g $1 -n $2
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t bootstrap-0.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master1.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master2.${2} -p 1 -w 1 -r 2380
-az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp.${2} -t master3.${2} -p 1 -w 1 -r 2380
+az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp -t bootstrap-0.${2} -p 1 -w 1 -r 2380
+az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp -t master1.${2} -p 1 -w 1 -r 2380
+az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp -t master2.${2} -p 1 -w 1 -r 2380
+az network private-dns record-set srv add-record -g $1 -z $2  -n _etcd-server-ssl._tcp -t master3.${2} -p 1 -w 1 -r 2380
 export MASTERIP=`az network public-ip show --resource-group $1 --name $1 --query [ipAddress] --output tsv`
 export APPIP=`az network public-ip show --resource-group $1 --name ${1}app --query [ipAddress] --output tsv`
 az network private-dns record-set a add-record -g $1 -z $2 -n master1 -a 10.0.0.5
